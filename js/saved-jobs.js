3e449b450941a92793dc329adf03a40d59f4d496
pri15ydv-cyber <pri15ydv@gmail.com>
2026-09-23 00:35:21 +0530

    Complete saved jobs functionality

--- js/saved-jobs.js
+++ js/saved-jobs.js
@@ -1,5 +1,23 @@
 const removeJobBtn = document.getElementById("removeJobBtn");
 
 removeJobBtn.addEventListener("click", function() {
+
+    const jobCard = removeJobBtn.closest(".card");
+
+    jobCard.remove();
+
     alert("Job removed from Saved Jobs.");
+
+});
+const savedJobDetailsBtn = document.getElementById("savedJobDetailsBtn");
+
+savedJobDetailsBtn.addEventListener("click", function() {
+
+    window.location.href = "job-details.html";
+
+});
+const savedJobApplyBtn = document.getElementById("savedJobApplyBtn");
+
+savedJobApplyBtn.addEventListener("click", function() {
+    window.location.href = "apply.html";
 });
